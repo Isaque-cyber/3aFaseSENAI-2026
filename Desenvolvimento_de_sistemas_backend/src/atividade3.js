e15452653d5903deffc1ca5e84fe248ce19683c3
ISAQUE RIBEIRO DA SILVA <isaque_r_silva@estudante.sesisenai.org.br>
2026-03-04 22:01:56 -0300

    fila e pilha

---
+++ Desenvolvimento_de_sistemas_backend/src/atividade3.js
@@ -0,0 +1,28 @@
+// 2. PILHAS (Stack – LIFO)
+// Exercício 3 — Validador de Parênteses
+// Enunciado
+// Verifique se uma expressão possui parênteses balanceados:
+// ((a+b)*c)
+
+function validarBalanceamento(expressao) {
+    const pilha = [];
+    const pares = {
+        ")": "(",
+        "]": "[",
+        "}": "{",
+
+    }
+    const quemAbre = new Set(["(", "[", '{'])
+
+    for (const caractere of expressao) {
+        if (quemAbre.has(caractere)) pilha.push(caractere)
+        else if (caractere in pares) {
+            if (pilha.length === 0) return false
+            const topo = pilha.pop()
+            if (topo !== pares[caractere]) return false
+        }
+    }
+
+    return pilha.length === 0;
+}
+console.log(validarBalanceamento("((a+b)(*c)"))
---
+++ Desenvolvimento_de_sistemas_backend/src/atividade_5.js
@@ -0,0 +1,25 @@
+
+
+
+class
+
+insertNoInicio(value){
+    const nodo = new Nodo(value)
+    nodo.next = this.ponteiro
+    this.ponteiro = nodo
+}
+
+mostrar(){
+    const output = []
+    const atual = atual.ponteiro
+    while(atual) {
+        output.push(atual)
+
+    }
+}
+
+const listaencadeada = new ListaEncadeada()
+
+listaencadeada.insertNoInicio(4)
+listaencadeada.insertNoInicio(5)
+listaencadeada.insertNoInicio(6)
